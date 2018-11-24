d05e49252b136e771431861c908331de85ecc564
Igor Rebega <igorrebega@gmail.com>
2018-11-24 21:19:24 +0200

    init

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -10,24 +10,24 @@ class CarController extends Controller
 {
     public function getInfo(Request $request)
     {
-        $number = $request->get('number');
-
-        $car = Car::query()->where(['number' => $number])->first();
-
-        if (! $car) {
-            throw new NotFoundHttpException();
-        }
+//        $number = $request->get('number');
+//
+//        $car = Car::query()->where(['number' => $number])->first();
+//
+//        if (! $car) {
+//            throw new NotFoundHttpException();
+//        }
 
         return [
-            'model'     => 'Skoda',
-            'brand'     => 'Fabia',
-            'year'      => 2012,
-            'color'     => 'Білий',
-            'body'      => 'Хеджбек',
-            'fuel'      => 'Дизель',
-            'capacity'  => '1580',
-            'weight'    => '1115',
-            'number'    => 'AA8631EH',
+            'model'     => 'Fabia',
+            'brand'     => 'Skoda',
+            'year'      => 'd',
+            'color'     => 'c',
+            'body'      => 'c',
+            'fuel'      => 'c',
+            'capacity'  => '1',
+            'weight'    => '1',
+            'number'    => 'c',
             'avg_price' => 0,
         ];
     }
